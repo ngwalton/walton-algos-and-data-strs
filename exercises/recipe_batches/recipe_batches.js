0f3d37270e6ca0416750e44e641e1d11327797ab
Nicholas Walton <ngwalton@gmail.com>
2024-03-16 14:42:03 -0500

    My solutions using reduce or map

--- exercises/recipe_batches/recipe_batches.js
+++ exercises/recipe_batches/recipe_batches.js
@@ -1,11 +1,35 @@
 /**
-It accepts two objects as arguments: the first object is the recipe
-for the food, while the second object is the available ingredients.
-Each ingredient's value is a number representing how many units there are.
+ * Question from:
+ * https://30secondsofinterviews.org/
+ */
 
-`batches(recipe, available)`
-*/
+/**
+ * Create a function batches that returns the maximum number of whole
+ * batches that can be cooked from a recipe:
+ * It accepts two objects as arguments: the first object is the recipe
+ * for the food, while the second object is the available ingredients.
+ * Each ingredient's value is a number representing how many units there are.
+ *
+ * `batches(recipe, available)`
+ */
 
-function batches() {}
+// recipe and available are both objects
+function batches(recipe, available) {
+  const minRatio = Object.keys(recipe).reduce(
+    (min, k) => Math.min(available[k] / recipe[k], min) || 0,
+    Infinity,
+  )
+
+  return Math.floor(minRatio)
+}
+
+// recipe and available are both objects
+// function batches(recipe, available) {
+//   const ratios = Object.entries(recipe).map(
+//     ([ingredient, amount]) => Math.floor(available[ingredient] / amount) || 0,
+//   )
+
+//   return Math.min(...ratios)
+// }
 
 module.exports = batches
