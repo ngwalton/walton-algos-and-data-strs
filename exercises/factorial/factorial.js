b35b11c263a94e4b098e02c6a3f29900e06d9b54
Nicholas Walton <ngwalton@gmail.com>
2024-03-23 11:20:45 -0500

    For loop solution to factorial

--- exercises/factorial/factorial.js
+++ exercises/factorial/factorial.js
@@ -1,3 +1,15 @@
-function factorial(n) {}
+// factorial returns n!
+// n is a positive integer
+// tried using BigInt for very large numbers, but jest doesn't seem to handle
+// this data type yet
+function factorial(n) {
+  let result = 1
+
+  for (let i = 1; i <= n; i++) {
+    result *= i
+  }
+
+  return result
+}
 
 module.exports = factorial
