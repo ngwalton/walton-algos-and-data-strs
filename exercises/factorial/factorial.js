c4e29de5203d3f9464bc4d4bc7142f1f20019170
Nicholas Walton <ngwalton@gmail.com>
2024-03-23 11:24:38 -0500

    Add recursive factorial

--- exercises/factorial/factorial.js
+++ exercises/factorial/factorial.js
@@ -2,14 +2,21 @@
 // n is a positive integer
 // tried using BigInt for very large numbers, but jest doesn't seem to handle
 // this data type yet
+
 function factorial(n) {
-  let result = 1
+  if (n === 0) return 1
 
-  for (let i = 1; i <= n; i++) {
-    result *= i
-  }
-
-  return result
+  return n * factorial(n - 1)
 }
+
+// function factorial(n) {
+//   let result = 1
+
+//   for (let i = 1; i <= n; i++) {
+//     result *= i
+//   }
+
+//   return result
+// }
 
 module.exports = factorial
